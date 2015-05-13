8b8857c4bc2d2c3dac24feaa367e0774925ad37b
Nathan Jones <thenathanjones@gmail.com>
2015-05-13 20:43:15 +1200

    Initial skeleton for Artifactory

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'artifactory'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures artifactory'
+long_description 'Installs/Configures artifactory'
+version          '0.1.0'
