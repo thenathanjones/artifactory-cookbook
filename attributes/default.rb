3a14f4afbffeda4d9e74b119157088b511335f16
Nathan Jones <thenathanjones@gmail.com>
2015-05-19 09:30:23 +1200

    Guard some of the sections in the XML

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,1 +1,5 @@
 default["artifactory"]["rpm_url"] = "http://bit.ly/Hqvfi9"
+default["artifactory"]["mail"] = {}
+default["artifactory"]["ldap"] = {}
+default["artifactory"]["external_url"] = {}
+default["artifactory"]["external_url"]["protocol"] = 'http'
